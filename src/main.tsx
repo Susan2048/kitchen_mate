65ba00f03c095d6acb0162baa64fd826a86eecb6
Susan2048 <156201710+Susan2048@users.noreply.github.com>
2025-11-03 13:31:57 +1300

    Add lightweight preview scaffolding

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import KitchenMateApp from './App';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <KitchenMateApp />
+  </React.StrictMode>
+);
